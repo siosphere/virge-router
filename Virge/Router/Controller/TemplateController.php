6fbeb3a9cc3a2d824273dcb62e5766d819067151
Kramer <kramer@siosphere.com>
2015-08-23 15:33:31 -0700

    Added base template controller

---
+++ Virge/Router/Controller/TemplateController.php
@@ -0,0 +1,30 @@
+<?php
+namespace Virge\Router\Controller;
+
+use Virge\Core\Config;
+use Virge\Router\Component\Response;
+use Virge\Router\Service\TemplateService;
+
+/**
+ * 
+ * @author Michael Kramer
+ */
+
+class TemplateController {
+    
+    /**
+     * 
+     * @param string $filepath
+     * @return Response
+     */
+    public function render($filepath) {
+        return $this->getTemplatingService()->render(Config::path($filepath));
+    }
+    
+    /**
+     * @return TemplateService
+     */
+    public function getTemplatingService() {
+        return Virge\Virge::service('templating');
+    }
+}
